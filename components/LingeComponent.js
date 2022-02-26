9f5915999071bedfde6284ecc63030d9d47312b6
Aymane Droussi <60798100+aymanedroussi@users.noreply.github.com>
2022-02-26 23:34:11 +0100

    Update LingeComponent.js

--- components/LingeComponent.js
+++ components/LingeComponent.js
@@ -60,6 +60,29 @@ Envoyé chez le sous traitant
 </td>
 
 <td className="p-2 whitespace-nowrap">
+{role=="ROLE_ADMIN" &&
+data.status=='En cours de traitement' && (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80">
+{data.status}
+</button>)}
+{role=="ROLE_ADMIN" &&
+data.status=='En cours de traitement sur place' && (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80">
+{data.status}
+</button>)}
+{role=="ROLE_ADMIN" &&
+data.status=='Envoyé chez le sous traitant' && (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80">
+{data.status}
+</button>)}
+{role=="ROLE_ADMIN" &&
+data.status=='traité'&& (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+{data.status}
+</button>)}
+{role=="ROLE_ADMIN" &&
+data.status=='remis'&& (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-green-600 rounded-md hover:bg-green-500 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-80">
+{data.status}
+</button>)}
+
+
+
 
 {role=="ROLE_MENAGE" &&
 data.status=='En cours de traitement' && (  <button disabled   className="marginbuttons px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-orange-600 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80">
