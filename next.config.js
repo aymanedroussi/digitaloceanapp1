1c897de5d833841873a945bd4a975942b5c72631
Aymane Droussi <60798100+aymanedroussi@users.noreply.github.com>
2022-02-26 22:22:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ module.exports = {
   env: {
     MONGODB_URI: "mongodb://localhost:27017/linge",
     MONGODB_PATIENT_URI:"mongodb://localhost:27017/dossierpatient",
-    SECRET:"LfsZWKdsKRwhz5bS7YOEI0A564a37saF",
-    NEXTAUTH_URL:"137.184.188.102"
+    SECRET:"LfsZWKdsKRwhz5bS7YOEI0A564a37saF"
     
     
   }
